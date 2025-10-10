a7856f169c43af4ac491b2271c5cef60ea47eafb
donia turki <belhoulaturki.donia@gmail.com>
2025-10-10 14:07:02 +0200

    ajout du fichier .env

--- src/main/java/fr/vod/config/WebConfig.java
+++ src/main/java/fr/vod/config/WebConfig.java
@@ -16,7 +16,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig {
 
 	@Value("${app.url}")
-	private String allowedOrigin;
+	private String allowedOrigin; // contient "http://localhost:3000,https://start-her.netlify.app"
 	
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -24,12 +24,14 @@ public class WebConfig {
         	
             @Override
             public void addCorsMappings(CorsRegistry registry) {
+            			// On split la chaîne en tableau pour accepter plusieurs origines
+                		String[] origins = allowedOrigin.split(",");
             			registry.addMapping("/**") // Allow all endpoints
-                        //.allowedOrigins("http://localhost:9091") // Allow React app origin
-                		.allowedOrigins(allowedOrigin)
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow HTTP methods
-                        .allowedHeaders("*") // Allow all headers
-                        .allowCredentials(true); // Allow credentials (cookies, etc.)
+                        		//.allowedOrigins("http://localhost:9091") // Allow React app origin
+            			 		.allowedOrigins(origins) // ici on passe le tableau
+                				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow HTTP methods
+                				.allowedHeaders("*") // Allow all headers
+                				.allowCredentials(true); // Allow credentials (cookies, etc.)
             }
         };
     }
